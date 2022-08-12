81e7fe5cd76299b638027e3708fb3f24d4d26561
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-08-12 15:45:38 +0000

    Add tests for single frame dataset

---
+++ tests/datasets/test_single_frame.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from h5py import File
+from numpy import iinfo, int32, split
+from numpy.random import randint
+from torch import from_numpy
+
+from ad_denoise.datasets.single_frames import SingleFrames
+
+
+def test_single_frame_produces_frames():
+    data = randint(iinfo(int32).max, size=(10, 10, 10))
+    with TemporaryDirectory() as tmpdir:
+        file_path = Path(tmpdir).joinpath("testfile.h5")
+        with File(file_path, "w") as file:
+            file["dataset"] = data
+        for idx, frame in enumerate(SingleFrames({(file_path, "dataset")})):
+            assert (from_numpy(data[idx]) == frame).all()
+
+
+def test_single_frame_produces_frames_multiple():
+    data = randint(iinfo(int32).max, size=(20, 10, 10))
+    data1, data2 = split(data, (8,))
+    with TemporaryDirectory() as tmpdir:
+        file_path1 = Path(tmpdir).joinpath("testfile1.h5")
+        with File(file_path1, "w") as file1:
+            file1["first"] = data1
+        file_path2 = Path(tmpdir).joinpath("testfile2.h5")
+        with File(file_path2, "w") as file2:
+            file2["second"] = data2
+        for idx, frame in enumerate(
+            SingleFrames([(file_path1, "first"), (file_path2, "second")])
+        ):
+            assert (from_numpy(data[idx]) == frame).all()
+
+
+def test_single_frame_reports_length():
+    data = randint(iinfo(int32).max, size=(20, 10, 10))
+    data1, data2 = split(data, (8,))
+    with TemporaryDirectory() as tmpdir:
+        file_path1 = Path(tmpdir).joinpath("testfile1.h5")
+        with File(file_path1, "w") as file1:
+            file1["first"] = data1
+        file_path2 = Path(tmpdir).joinpath("testfile2.h5")
+        with File(file_path2, "w") as file2:
+            file2["second"] = data2
+        assert 20 == len(SingleFrames([(file_path1, "first"), (file_path2, "second")]))
