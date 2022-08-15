18c4c5aa3880ba6caa52007fd93479967f43e31a
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-08-15 15:40:16 +0000

    Simplify frame pair dataset implementations

--- src/ad_denoise/datasets/different_pairs.py
+++ src/ad_denoise/datasets/different_pairs.py
@@ -1,7 +1,8 @@
-from torch import Tensor, float32, from_numpy
+from torch import Tensor
 from torch.utils.data import Dataset
 
-from .utils import H5Key, H5Paths, get_dataset_edges, open_datasets, read_frame_datasets
+from .single_frames import SingleFrames
+from .utils import H5Key, H5Paths
 
 
 class MatchedFramePairs(Dataset):
@@ -32,40 +33,20 @@ class MatchedFramePairs(Dataset):
             left_frame_dims: The trailing dimensionality of the frame. Defaults to 2.
             right_frame_dims: The trailing dimensionality of the frame. Defaults to 2.
         """
-        self.left_frame_dims = left_frame_dims
-        self.right_frame_dims = right_frame_dims
-        (self.left_datasets,) = open_datasets(left_paths, (left_frames_key,))
-        (self.right_datasets,) = open_datasets(right_paths, (right_frames_key,))
-        self.left_edges = get_dataset_edges(self.left_datasets, self.left_frame_dims)
-        self.right_edges = get_dataset_edges(self.right_datasets, self.right_frame_dims)
-        if self.left_edges[-1] != self.right_edges[-1]:
+        self.left_dataset = SingleFrames(left_paths, left_frames_key, left_frame_dims)
+        self.right_dataset = SingleFrames(
+            right_paths, right_frames_key, right_frame_dims
+        )
+        if len(self.left_dataset) != len(self.right_dataset):
             raise ValueError(
                 "Left & right datasets must contain the same number of frames."
             )
 
     def __len__(self) -> int:
-        return self.left_edges[-1]
+        return len(self.left_dataset)
 
     def __getitem__(self, idx: int) -> tuple[Tensor, Tensor]:
-        left_frame = (
-            from_numpy(
-                read_frame_datasets(
-                    self.left_datasets, idx, self.left_frame_dims, self.left_edges
-                )
-            )
-            .unsqueeze(0)
-            .type(float32)
-        )
-        right_frame = (
-            from_numpy(
-                read_frame_datasets(
-                    self.right_datasets, idx, self.right_frame_dims, self.right_edges
-                )
-            )
-            .unsqueeze(0)
-            .type(float32)
-        )
-        return left_frame, right_frame
+        return self.left_dataset[idx], self.right_dataset[idx]
 
 
 class CrossedFramePairs(Dataset):
@@ -96,40 +77,15 @@ class CrossedFramePairs(Dataset):
             left_frame_dims: The trailing dimensionality of the frame. Defaults to 2.
             right_frame_dims: The trailing dimensionality of the frame. Defaults to 2.
         """
-        super().__init__()
-        self.left_frame_dims = left_frame_dims
-        self.right_frame_dims = right_frame_dims
-        (self.left_datasets,) = open_datasets(left_paths, (left_frames_key,))
-        (self.right_datasets,) = open_datasets(right_paths, (right_frames_key,))
-        self.left_edges = get_dataset_edges(self.left_datasets, self.left_frame_dims)
-        self.right_edges = get_dataset_edges(self.right_datasets, self.right_frame_dims)
+        self.left_dataset = SingleFrames(left_paths, left_frames_key, left_frame_dims)
+        self.right_dataset = SingleFrames(
+            right_paths, right_frames_key, right_frame_dims
+        )
 
     def __len__(self) -> int:
-        return self.left_edges[-1] * self.right_edges[-1]
+        return len(self.left_dataset) * len(self.right_dataset)
 
     def __getitem__(self, idx: int) -> tuple[Tensor, Tensor]:
-        left_idx = idx // self.left_edges[-1]
-        right_idx = idx % self.left_edges[-1]
-
-        left_frame = (
-            from_numpy(
-                read_frame_datasets(
-                    self.left_datasets, left_idx, self.left_frame_dims, self.left_edges
-                )
-            )
-            .unsqueeze(0)
-            .type(float32)
-        )
-        right_frame = (
-            from_numpy(
-                read_frame_datasets(
-                    self.right_datasets,
-                    right_idx,
-                    self.right_frame_dims,
-                    self.right_edges,
-                )
-            )
-            .unsqueeze(0)
-            .type(float32)
-        )
-        return left_frame, right_frame
+        left_idx = idx // len(self.left_dataset)
+        right_idx = idx % len(self.left_dataset)
+        return self.left_dataset[left_idx], self.right_dataset[right_idx]
