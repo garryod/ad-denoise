19c598c7a32ba8ece60ca7a198bc3870bd9a41b3
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-08-19 17:10:58 +0000

    Add train command to CLI

--- src/ad_denoise/cli.py
+++ src/ad_denoise/cli.py
@@ -1,4 +1,11 @@
+from dataclasses import dataclass
+from pathlib import Path
+
 import click
+from pytorch_lightning import Trainer
+
+from ad_denoise.lightning_modules import LightningModuleConfig
+from ad_denoise.utils import load_config
 
 from ._version_git import __version__
 
@@ -14,3 +21,19 @@ def main(ctx: click.Context) -> None:
     """
     if ctx.invoked_subcommand is None:
         click.echo(main.get_help(ctx))
+
+
+@dataclass
+class TrainConfig:
+    """A configuration schema for network training."""
+
+    model: LightningModuleConfig
+    max_epochs: int
+
+
+@main.command(help="Train a model on the given datasets")
+@click.argument("config_file", type=click.Path(exists=True, dir_okay=False))
+def train(config_file: Path) -> None:  # noqa: D103
+    config = load_config(config_file, TrainConfig)
+    trainer = Trainer(max_epochs=config.max_epochs, log_every_n_steps=1)
+    trainer.fit(config.model())
