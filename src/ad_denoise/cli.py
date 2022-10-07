cc0371cb660dbdfa7fd32f798b43c7be06f327a7
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-10-07 14:09:07 +0000

    Configure log names

--- src/ad_denoise/cli.py
+++ src/ad_denoise/cli.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from os import getcwd
 from pathlib import Path
 
 import click
 from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
 
 from ad_denoise.lightning_modules import LightningModuleConfig
 from ad_denoise.utils import load_config
@@ -27,6 +29,7 @@ def main(ctx: click.Context) -> None:
 class TrainConfig:
     """A configuration schema for network training."""
 
+    name: str
     model: LightningModuleConfig
     max_epochs: int
 
@@ -35,7 +38,11 @@ class TrainConfig:
 @click.argument("config_file", type=click.Path(exists=True, dir_okay=False))
 def train(config_file: Path) -> None:  # noqa: D103
     config = load_config(config_file, TrainConfig)
+    logger = TensorBoardLogger(str(Path(getcwd()).joinpath("logs")), config.name)
     trainer = Trainer(
-        max_epochs=config.max_epochs, log_every_n_steps=1, accelerator="auto"
+        max_epochs=config.max_epochs,
+        log_every_n_steps=1,
+        accelerator="auto",
+        logger=logger,
     )
     trainer.fit(config.model())
