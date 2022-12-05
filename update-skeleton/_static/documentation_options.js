8097a65202f3caad56c2ea34b3b573d3aa7cbefb
garryod <garryod@users.noreply.github.com>
2022-12-05 16:45:37 +0000

    deploy: e9ce33da8ccb565297c09583b4e3df4ac0921c65

--- update-skeleton/_static/documentation_options.js
+++ update-skeleton/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev120+gc5ecc8b',
+    VERSION: '0.1.dev119+ge9ce33d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
